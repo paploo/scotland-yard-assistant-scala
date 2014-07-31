63f5ced8ee263e9567099db657e29327f0f86324
Jeffrey Reinecke <jreinecke@whitepages.com>
2014-07-30 18:18:55 -0700

    Board: DSL works now

--- src/main/scala/net/paploo/scotlandyard/SYApp.scala
+++ src/main/scala/net/paploo/scotlandyard/SYApp.scala
@@ -1,7 +1,8 @@
 package net.paploo.scotlandyard
 
-import net.paploo.scotlandyard.board.Route.{Taxi, Bus}
+import net.paploo.scotlandyard.board.Route.TransitMode.{Taxi, Bus}
 import net.paploo.scotlandyard.board.{Board, Route, Station}
+import net.paploo.scotlandyard.board.Board._
 import net.paploo.scotlandyard.graph._
 
 object SYApp {
@@ -10,24 +11,26 @@ object SYApp {
     val nodes: Seq[Node[Station, Route]] = List(1,2,3).map( Station(_).toNode )
 
     val edges: Seq[Edge[Station, Route]] = List(
-      Taxi.toEdge(1, 2),
-      Taxi.toEdge(2, 1),
-      Bus.toEdge(1, 3),
-      Bus.toEdge(3, 1),
-      Taxi.toEdge(2, 3),
-      Taxi.toEdge(3, 2)
-    )
+      Route(1, 2, Taxi),
+      Route(2, 1, Taxi),
+      Route(2, 1, Taxi),
+      Route(1, 3, Bus),
+      Route(3, 1, Bus),
+      Route(2, 3, Taxi),
+      Route(3, 2, Taxi)
+    ).map(_.toEdge)
 
-    implicit val graph = Board(nodes, edges)
+    implicit val graph: Graph[Station, Route] = Board(nodes, edges)
 
-    val path: Path[Station,Route] = Path(Seq(NodeID(2)))
+    val paths = Board.startingPaths(List(2))
 
-    //val p2 = path.transition(_.data == Route("Taxi"))
-    //println(p2.map(_.headOption.map(_.id).get))
-
-    val p3 = Seq(path).transitionEdges(_.data == Taxi).transitionEdges(_.data == Bus).filterNodes(_.data.num != 1).transitionEdges(_.data == Taxi)
+    val p3 = paths.transitionEdges(_.data.transitMode == Taxi).transitionEdges(_.data.transitMode == Bus).filterNodes(_.data.num != 1).transitionEdges(_.data.transitMode == Taxi)
     println(p3)
     println(p3.headNodeOption.map(_.get.id))
+
+    val p4 = paths.moveVia(Taxi).moveVia(Bus).detectiveAt(1).moveVia(Taxi)
+    println(p4)
+    println(p4.headNodeOption.map(_.get.id))
   }
 
 }
--- src/main/scala/net/paploo/scotlandyard/board/Board.scala
+++ src/main/scala/net/paploo/scotlandyard/board/Board.scala
@@ -1,22 +1,46 @@
 package net.paploo.scotlandyard.board
 
-import net.paploo.scotlandyard.graph.{Graph, Node, Edge, NodeID}
-
-sealed trait Route {
-  def toEdge(sourceNum: Int, destinationNum: Int): Edge[Station, Route] = new Edge(NodeID(sourceNum), NodeID(destinationNum), this)
-}
+import net.paploo.scotlandyard.board.Route.TransitMode
+import net.paploo.scotlandyard.graph._
 
 object Route {
-  case object Taxi extends Route
-  case object Bus extends Route
-  case object Underground extends Route
-  case object Ferry extends Route
+
+  sealed trait TransitMode
+
+  object TransitMode {
+    case object Taxi extends TransitMode
+    case object Bus extends TransitMode
+    case object Underground extends TransitMode
+    case object Ferry extends TransitMode
+  }
+
 }
+
+case class Route(sourceNum: Int, destinationNum: Int, transitMode: Route.TransitMode) {
+  def toEdge: Edge[Station, Route] = new Edge(NodeID(sourceNum), NodeID(destinationNum), this)
+}
+
 
 case class Station(num: Int) {
   def toNode: Node[Station, Route] = new Node(NodeID(num), this)
 }
 
 object Board {
+
   def apply[N <: Station, E <: Route](nodes: Seq[Node[N,E]], edges: Seq[Edge[N,E]]): Graph[N,E] = new Graph(nodes, edges)
+
+  def startingNodeIDs(startingNums: Seq[Int]): Seq[NodeID] = startingNums.map(Station(_).toNode.id)
+
+  def startingPaths(startingNums: Seq[Int]): Seq[Path[Station,Route]] = startingNodeIDs(startingNums).map(id => new Path(List(id)))
+
+  implicit class BoardPath(val paths: Seq[Path[Station, Route]]) {
+
+    def moveVia(transitMode: TransitMode)(implicit graph: Graph[Station, Route]): Seq[Path[Station, Route]] = paths.transitionEdges(_.data.transitMode == transitMode)
+
+    def detectiveAt(stationNum: Int)(implicit graph: Graph[Station, Route]): Seq[Path[Station, Route]] = paths.filterNodes(_.data.num != stationNum)
+
+    def mrXAt(stationNum: Int)(implicit graph: Graph[Station, Route]): Seq[Path[Station, Route]] = paths.filterNodes(_.data.num == stationNum)
+
+  }
+
 }

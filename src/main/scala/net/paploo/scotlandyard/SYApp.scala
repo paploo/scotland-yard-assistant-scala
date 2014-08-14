0ea13b2b5efa783a8c36729d1b2837cba6653476
Jeffrey Reinecke <jreinecke@whitepages.com>
2014-08-13 23:50:20 -0700

    Command/AppLoop restructuring.

--- src/main/scala/net/paploo/scotlandyard/SYApp.scala
+++ src/main/scala/net/paploo/scotlandyard/SYApp.scala
@@ -14,8 +14,8 @@ object SYApp {
   case class GameState(board: Board, paths: Seq[Path[Station, Route]])
 
   var games: Map[String, GameState] = Map()
-  var currentGameKey: Option[String] = None
-  def currentGame: Option[GameState] = currentGameKey.flatMap(k => games.get(k))
+  var currentGameRef: Option[String] = None
+  def currentGame: Option[GameState] = currentGameRef.flatMap(k => games.get(k))
 
   def main(args: Array[String]): Unit = {
     mainLoop()
@@ -32,37 +32,15 @@ object SYApp {
   }
 
   protected def prompt: String = {
-    val fmtCurrGameKey = currentGameKey match {
+    val fmtCurrGameRef = currentGameRef match {
       case Some(k) => s"${Console.BLUE}$k${Console.RESET}"
       case None => s"(${Console.YELLOW}no game${Console.RESET})"
     }
-    s"$fmtCurrGameKey> "
+    s"$fmtCurrGameRef> "
   }
 
   protected def processCommand(cmd: Command): Unit = {
-    //TODO: Match should return Option[String], and we log if it has a value.
-    cmd match {
-      case Quit => System.exit(0)
-      case NewGame(edition, ref) =>
-        val board = Board(edition).get
-        val game = GameState(board, board.startingPaths)
-        games = games + (ref -> game)
-        currentGameKey = Some(ref)
-      case SetGame(ref) =>
-        currentGameKey = Some(ref)
-      case ListGames =>
-        Console.println(games.keys.mkString("  ", "\n  ", ""))
-      case GetPaths(None) =>
-        currentGame.map(game => Console.println(formatPaths(game.paths)(game.board.graph)))
-      case GetPaths(Some(ref)) =>
-        games.get(ref).map(game => Console.println(formatPaths(game.paths)(game.board.graph)))
-      case MoveVia(ticket) =>
-        currentGame.map(game => game.paths.moveVia(ticket)(game.board.graph)) //TODO: Need to actually write the board to the game state!
-      case DetectiveAt(stationNum: Int) =>
-        currentGame.map(game => game.paths.detectiveAt(stationNum)(game.board.graph)) //TODO: Need to actually write the board to the game state!
-      case MrXAt(stationNum: Int) =>
-        currentGame.map(game => game.paths.mrXAt(stationNum)(game.board.graph)) //TODO: Need to actually write the board to the game state
-    }
+    performCommand(cmd).map(Console.println(_))
     Console.println(s"=> ${Console.GREEN}Done: $cmd${Console.RESET}\n")
   }
 
@@ -70,7 +48,61 @@ object SYApp {
     Console.println(s"=> ${Console.RED}ERROR: ${err.getMessage}${Console.RESET}")
   }
 
+  protected def performCommand(cmd: Command): Option[String] = cmd match {
+    case Quit =>
+      System.exit(0)
+      None
+    case NewGame(edition, ref) =>
+      val board = Board(edition).get
+      val game = GameState(board, board.startingPaths)
+      games = games + (ref -> game)
+      currentGameRef = Some(ref)
+      Some(s"Game created with id $ref.")
+    case SetGame(ref) =>
+      currentGameRef = Some(ref)
+      Some(s"Set game to id $ref.")
+    case ListGames =>
+      Some(games.keys.mkString("  ", "\n  ", ""))
+    case GetPaths(None) =>
+      currentGame.map(game => formatPaths(game.paths)(game.board.graph))
+    case GetPaths(Some(ref)) =>
+      games.get(ref).map(game => formatPaths(game.paths)(game.board.graph))
+    case MoveVia(ticket) =>
+      currentGame.flatMap(game => updateCurrentPaths(game.paths.moveVia(ticket)(game.board.graph))).flatMap(_ => performCommand(GetPaths(None)))
+    case DetectiveAt(stationNum: Int) =>
+      currentGame.flatMap(game => updateCurrentPaths(game.paths.detectiveAt(stationNum)(game.board.graph))).flatMap(_ => performCommand(GetPaths(None)))
+    case MrXAt(stationNum: Int) =>
+      currentGame.flatMap(game => updateCurrentPaths(game.paths.mrXAt(stationNum)(game.board.graph))).flatMap(_ => performCommand(GetPaths(None)))
+  }
+
   protected def formatPaths(paths: Seq[Path[Station, Route]])(implicit graph: Graph[Station,Route]): String =
     paths.map(path => path.nodeIDs.map(id => graph(id).data.num).map(num => f"$num%3d").mkString(", ")).mkString("\n")
+
+  protected def updateCurrentPathsFunctional(newPaths: => Seq[Path[Station, Route]]): Option[GameState] = currentGameRef.flatMap { ref =>
+    currentGame.map { game =>
+      val newGame = game.copy(paths = newPaths)
+      games = games + (ref -> newGame)
+      newGame
+    }
+  }
+
+  protected def updateCurrentPaths(pathComputer: Function[GameState, Path[Station, Route]]): Option[GameState] = for {
+    ref <- currentGameRef
+    game <- currentGame
+    newPaths <- pathComputer(game)
+    newGame = game.copy(paths = newPaths)
+  } yield {
+    games = games + (ref -> newGame)
+    newGame
+  }
+
+  protected def updateCurrentPaths(newPaths: => Seq[Path[Station, Route]]): Option[GameState] = for {
+    ref <- currentGameRef
+    game <- currentGame
+    newGame = game.copy(paths = newPaths)
+  } yield {
+    games = games + (ref -> newGame)
+    newGame
+  }
 
 }
